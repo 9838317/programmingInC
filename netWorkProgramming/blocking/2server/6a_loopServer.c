dab420b85b2eb8b7c30e19faa8be9b769288864d
9838317@qq.com <9838317@qq.com>
2017-05-12 04:41:29 -0400

    automatic commit

--- netWorkProgramming/blocking/2server/6a_loopServer.c
+++ netWorkProgramming/blocking/2server/6a_loopServer.c
@@ -8,11 +8,12 @@ int main(int argc, char** argv)
 
     PORT = atoi(argv[1]);
     serverfd = getserverfd(PORT);
-    
+    printf("%s %d \n", "listing file descriptor is", serverfd);  
     while(1)
     {
         clientlen = sizeof(clientSocket); 
         confd     = accept(serverfd, (SA* )&clientSocket, &clientlen);
+        printf("%s %d \n", "accepting file descriptor is", confd);  
         printf("%s \n", "connected!");
         write(confd, "string", 6);
         close(confd);
